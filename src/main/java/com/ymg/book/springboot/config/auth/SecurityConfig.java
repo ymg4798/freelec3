f65272610bc0d8f1830bd980fc8a246e8757fc32
ymg4798 <ymg4798@naver.com>
2020-09-21 16:14:10 +0900

    gitignore 등록

---
+++ src/main/java/com/ymg/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.ymg.book.springboot.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2UserService;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                    .and()
+                        .authorizeRequests()
+                        .antMatchers("/")
+    }
+
+}
